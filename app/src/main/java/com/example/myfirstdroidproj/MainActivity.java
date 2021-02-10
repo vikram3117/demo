baea5a2f0714ad302f0af051c55a18162aafbafc
vikram3117 <vikramsingh7237@gmail.com>
2021-02-11 01:02:32 +0530

    okay

--- app/src/main/java/com/example/myfirstdroidproj/MainActivity.java
+++ app/src/main/java/com/example/myfirstdroidproj/MainActivity.java
@@ -6,8 +6,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,45 +22,39 @@ import com.google.firebase.database.ValueEventListener;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final EditText x=findViewById(R.id.firstnum);
-        final EditText y=findViewById(R.id.secondnum);
-        final TextView z=findViewById(R.id.sum);
-        Button yo=findViewById(R.id.button);
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        final DatabaseReference myRef = database.getReference("values");
-        yo.setOnClickListener(new View.OnClickListener() {
+        final ListView li = findViewById(R.id.list);
+        ArrayList<User> arr = new ArrayList<User>();
+        arr.add(new User(1, "Aryan Khanna"));
+        arr.add(new User(2, "Vikram"));
+
+        ArrayAdapter<String> ad = new ArrayAdapter<String>(this, R.layout.my_layout,R.id.text, getstringarray(arr));
+        li.setAdapter(ad);
+        li.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View v)
-            {
-//                int lol=Integer.parseInt(x.getText().toString())+Integer.parseInt(y.getText().toString());
-//                Log.e("hiiiiiii!!!!",Integer.parseInt(x.getText().toString())+"");
-////                z.setText(""+lol);
-//                myRef.child("value1").setValue(x.getText().toString());
-//                myRef.child("value2").setValue(y.getText().toString());
-                myRef.setValue(new Values(Integer.parseInt(x.getText().toString()),Integer.parseInt(y.getText().toString())));
-
-                myRef.addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot snapshot) {
-                        Values val = snapshot.getValue(Values.class);
-                        int bkl=val.val1+val.val2;
-                        z.setText(""+bkl);
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) {
-
-                    }
-                });
-
-
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String input = (String) li.getItemAtPosition(position);
+                Toast.makeText(MainActivity.this, input, Toast.LENGTH_LONG).show();
             }
         });
+
+
+    }
+    public ArrayList<String> getstringarray(ArrayList<User> arr)
+    {
+        ArrayList<String> ans=new ArrayList<String>();
+        for(User i:arr)
+        {
+            ans.add(i.name);
+        }
+        return ans;
     }
 }
+
---
+++ app/src/main/java/com/example/myfirstdroidproj/User.java
@@ -0,0 +1,18 @@
+package com.example.myfirstdroidproj;
+
+public class User
+{
+
+    public int id;
+    public String name;
+    User (int a,String b)
+    {
+        this.id=a;
+        this.name=b;
+    }
+    User ()
+    {
+        id=0;
+        name="";
+    }
+}
